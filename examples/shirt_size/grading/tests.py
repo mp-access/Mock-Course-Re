984f756be3de6b160efb942e625941d0a7d14067
Carol Alexandru <alexandru@ifi.uzh.ch>
2025-08-27 05:15:54 +0200

    minor improvements to grading tests

--- examples/shirt_size/grading/tests.py
+++ examples/shirt_size/grading/tests.py
@@ -14,52 +14,61 @@ DELTA = 0.0001
 
 class GradingTests(AccessTestCase):
 
-    def _call(self, circumference):
+    def call(self, circumference, suffix=""):
         try:
             return script.get_size(circumference)
-        except:
-            self.hint(f"Solution crashes when called with {circumference}")
+        except Exception as e:
+            self.hint(f"Solution crashes with '{type(e).__name__}' when called with {circumference}{suffix}".strip())
             self.fail()
- 
-    def _test(self, circumference, expected):
-        actual = self._call(circumference)
+
+    def assertShirtSize(self, circumference, expected):
+        actual = self.call(circumference)
         self.hint(f"Your determined size {actual} for a circumference of {circumference} is not correct! The correct size is: {expected}.")
         self.assertEqual(expected, actual)
-        
+
+    def assertShirtSizeFloat(self, circumference, expected):
+        suffix = ", which is a floating point number. Are you perhaps using the range() function?"
+        actual = self.call(circumference, suffix)
+        self.hint(f"Your solution returns {actual} for a circumference of {circumference}{suffix} The correct size is: {expected}")
+        self.assertEqual(expected, actual)
+ 
     def test_XS(self):
-        self._test(80, "XS")
-        self._test(80+DELTA, "XS")
-        self._test(90 - DELTA, "XS")
-        self._test(90, "XS")
+        self.assertShirtSize(80, "XS")
+        self.assertShirtSize(90, "XS")
 
     def test_S(self):
-        self._test(90 + DELTA, "S")
-        self._test(98 - DELTA, "S")
-        self._test(98, "S")
+        self.assertShirtSize(98, "S")
 
     def test_M(self):
-        self._test(98 + DELTA, "M")
-        self._test(104 - DELTA, "M")
-        self._test(104, "M")
+        self.assertShirtSize(104, "M")
 
     def test_L(self):
-        self._test(104 + DELTA, "L")
-        self._test(111 - DELTA, "L")
-        self._test(111, "L")
+        self.assertShirtSize(111, "L")
 
     def test_XL(self):
-        self._test(111 + DELTA, "XL")
-        self._test(124 - DELTA, "XL")
-        self._test(124, "XL")
+        self.assertShirtSize(124, "XL")
 
-    
     def test_NA(self):
-        self._test(80 - DELTA, "N/A")
-        self._test(124 + DELTA, "N/A")
-    
+        self.assertShirtSize(80 - 1, "N/A")
+        self.assertShirtSize(124 + 1, "N/A")
+
+    @weight(2)
+    def test_floats(self):
+        self.assertShirtSizeFloat(80 + DELTA, "XS")
+        self.assertShirtSizeFloat(90 - DELTA, "XS")
+        self.assertShirtSizeFloat(90 + DELTA, "S")
+        self.assertShirtSizeFloat(98 - DELTA, "S")
+        self.assertShirtSizeFloat(98 + DELTA, "M")
+        self.assertShirtSizeFloat(104 - DELTA, "M")
+        self.assertShirtSizeFloat(104 + DELTA, "L")
+        self.assertShirtSizeFloat(111 - DELTA, "L")
+        self.assertShirtSizeFloat(111 + DELTA, "XL")
+        self.assertShirtSizeFloat(124 - DELTA, "XL")
+        self.assertShirtSizeFloat(80 - DELTA, "N/A")
+        self.assertShirtSizeFloat(124 + DELTA, "N/A")
 
     @weight(0)
-    def test_correct_or_multiple_sizes_wrong(self):
+    def test_symptom_one_edge_case_missing(self):
         # naming hard to get right / intuitive
         failures = []
 
@@ -77,53 +86,47 @@ class GradingTests(AccessTestCase):
                 test_func()  
             except AssertionError as e:
                 failures.append(f"{test_func.__name__}: {e}")
-            except Exception as e:
-                failures.append(f"{test_func.__name__}: crashed ({type(e).__name__}: {e})")
 
         total_failures = len(failures)
         print(f"TOTAL FAILURE: {total_failures}" )
-        self.assertNotEqual(total_failures, 1) 
+        self.assertNotEqual(total_failures, 1)
 
 
     @weight(0)
     def test_correct_boundary_handling(self):
-        self._test(80 - DELTA, "N/A")
+        self.assertShirtSize(80 - DELTA, "N/A")
 
-        self._test(80+DELTA, "XS")        
-        self._test(90 - DELTA, "XS")
+        self.assertShirtSize(80 + DELTA, "XS")
+        self.assertShirtSize(90 - DELTA, "XS")
 
-        self._test(90 + DELTA, "S")
-        self._test(98 - DELTA, "S")
-        
-        self._test(98 + DELTA, "M")
-        self._test(104 - DELTA, "M")
-        
-        self._test(104 + DELTA, "L")
-        self._test(111 - DELTA, "L")
-        
-        self._test(111 + DELTA, "XL")
-        self._test(124 - DELTA, "XL")
+        self.assertShirtSize(90 + DELTA, "S")
+        self.assertShirtSize(98 - DELTA, "S")
 
-        self._test(124 + DELTA, "N/A")
+        self.assertShirtSize(98 + DELTA, "M")
+        self.assertShirtSize(104 - DELTA, "M")
 
-    def _check_float_as_input(self, circumference, expected):
-        actual = self._call(circumference)
-        self.hint(f"Your solution doesn't work for floating point numbers. Your determined size {actual} for a circumference of {circumference} is not correct! The correct size is: {expected}. Are you perhaps using the range() function? ")
-        self.assertEqual(expected, actual)
-    
+        self.assertShirtSize(104 + DELTA, "L")
+        self.assertShirtSize(111 - DELTA, "L")
+
+        self.assertShirtSize(111 + DELTA, "XL")
+        self.assertShirtSize(124 - DELTA, "XL")
+
+        self.assertShirtSize(124 + DELTA, "N/A")
+
+   
     @weight(0)
     def test_handles_floats(self):
-        # chose floats that for sure fail bc. they are float, not because of wrong boundaries
-        self._check_float_as_input(82.5, "XS")
-        self._check_float_as_input(92.5, "S")
-        self._check_float_as_input(100.5, "M")
-        self._check_float_as_input(106.5, "L")
-        self._check_float_as_input(113.5, "XL")
+        # choose floats that for sure fail bc. they are float, not because of wrong boundaries
+        self.assertShirtSizeFloat(82.5, "XS")
+        self.assertShirtSizeFloat(92.5, "S")
+        self.assertShirtSizeFloat(100.5, "M")
+        self.assertShirtSizeFloat(106.5, "L")
+        self.assertShirtSizeFloat(113.5, "XL")
         
     @weight(0)
     def test_implementation_runs(self):
         one_circumference_per_size = [81,91,99,105, 112]
         for circumference in one_circumference_per_size:
-            self._call(circumference)
+            self.call(circumference)
 
 TestRunner().run(AccessTestSuite(1, [GradingTests]))
